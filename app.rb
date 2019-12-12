f34696127018caad2ebcc3933843821b7918554a
Srikalyan Megati <kalmegati@gmail.com>
2019-12-12 15:36:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/' do
-    erb :
+    erb :user_input
   end
   
   post '/piglatinize' do
+    @text = params["user_input"]
     
+    erb :web_output
   end
   
 end
